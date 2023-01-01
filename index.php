edaebbe57b4b7e4775dd9e83a2db89f28c3d768f
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-01 11:11:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
 <head>
     <title>Ma page web</title>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+
 </head>
 <body>
 <h1>Bienvenue sur ma page web</h1>
 <p>Voici un paragraphe de contenu</p>
+<img src="images/banner.jpg" alt="apprendre a faire du violon">
 </body>
 </html>
