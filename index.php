fa468e44c5bb46160affb44c7af338a83c3f79f0
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-01 18:23:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html>
-<?php
-  echo "<img src=images/banner.jpeg>";
-?>
-
 
 <head>
     <title>Ma page web</title>
@@ -13,8 +9,52 @@
 </head>
 <body>
 <h1>Delite Music </h1>
+<h2>Des videos de musics disponibles en integralites </h2>
 <p> Des videos de musics disponibles en integralites <br>
 Que vous pouvez telechargez pour apprendre d'un instrument de musique </p>
+<h3> A prix abordables et de tout genres </h3>
+<p> la passion d'appendre d'un instrument  </p>
+<p> Jouer d'un instrument de musique n'a jamais ete ausi simple, maintenant que les tutoriels en lignes <br> existent </p>
+<p> Profiter desormais de plusieurs essais </p>
+<?php
+ 
+  echo "<img src = database/images/piano.jpg  alt apprendre le piano en ligne";
+  
+
+?>
+
+ <?php
+  echo "<img src =https://www.istockphoto.com/fr/photo/guitare-classique-sur-fond-de-bois-sombre-gm1322201765-408238515  alt apprendre le piano en ligne";
+  
+?>
+<br>
+<?php
+  echo "<img https://unsplash.com/fr/photos/C8gib_msapY"
+  
+?>
+<br>
+<?php
+  echo "<img https://www.istockphoto.com/fr/photo/vieux-violon-sur-blanc-gm184329350-17400164"
+  
+?>
+<br>
+<?php
+  echo "<img https://www.istockphoto.com/fr/photo/violoncelle-complet-du-corps-plan-gm155394947-20052339?phrase=violoncelle"
+  
+?>
+<br>
+<h3> Choissisez la video qui vous interesse </h3>
+
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+
+
+
+
 
 
 
