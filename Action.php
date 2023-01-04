c3b6e299920f673635eb8f1cc1c8755f7cbf07f1
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-04 19:38:06 +0100

    Update Action.php

--- Action.php
+++ Action.php
@@ -1,13 +1,42 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Titre de la page</title>
+    <title>site-action</title>
     <?php echo '<link rel="stylesheet" href="styleAction.css">'; ?>
     <meta charset="UTF-8">
     <meta charset="utf-8">
   </head>
   <body>
-    <h1>Titre de la page</h1>
-    <p>Contenu de la page</p>
+    <h1>Action</h1>
+    <p>des videos basees sur le theme de l'action. Avec ceci, vous pourrez en apprendre bien plus. Les videos sont toujours bases sur l'apprentisages d'un instrument de musique, cequi vous permettra de vous ameliorez ou bien de commencez son etude.</p>
+    <br>
+    <h2>Divers instruments de musiques vous sont proposes en rapport avec l'action</h2>
+    <br>
+    <p> A vous de choisir la video qui vous convient. Vous pouvez optez pour le violocelle, la guitare, le piano ou meme le violon. Tout cela regroupe a petit forma </p>
+    <p> Avez-vous parfois le sentiment d'etre perdu sur internet face a la quantite de partitions et de videos mides a dispositions ? Comment vous orientez vous sur le web
+      en ayant confiance dans son contenu ? </p>
+   <br>
+    <p>Sur ce site, vous decouvrirez des videos qui parlent principalement d'instrument. Ces instruments de musiques s'adresse egalement aux amateurs melomanes </p>
+    <p> En parcourant toutes les pages, vous profiterez de l'expertise et des investigations qui met ses competences et son energie à votre service dans le but de partager la passion de son metier en vous aidant à trouver votre chemin dans le labyrinthe d'internet </p>
+    <br>
+    <p> Souhaitez -vous recuperez facilement des partitions gratuites et libres d'acces organises par epoques et par niveaux de difficulté ? vous profiterez également d'enregistrements de qualité associés à une grande partie des oeuvres d'actions pour mieux vous decider et mieux les comprendre </p>
+      <h2>"Entrez dans le Partiontheque" </h2>
+      <p> Vous ne trouvez pas les morceaux de votre choix ? Consultez la Partitionthèque qui regroupe toutes les partitions digitales du répertoire que vous pourrez télécharger en quelques minutes seulement.</p>
+    <h2> "Visionnez les videos" </h2>
+    <p>​Souhaitez-vous développer votre culture musicale du violon en visionnant des vidéos consacrées aux masterclasses, aux interviews et aux concerts donnés par les plus grands violonistes depuis près d'un siècle ?  </p>
+    <hr>
+    <h3> titre : Crazy In Love - Fifty Shades of Grey | Easy PIANO TUTORIAL
+    <video src = "https://www.youtube.com/watch?v=pKdMhkciwrs&list=PL6urkeK7KgD41JDZRHaQzdKlHrJ5-fvdr&index=1" controls></video>       <!-- ajout de la video -->
+    <h3> prix : 4.50 €
+      <br>
+      realisateur : Alison Sparrow : theme : Action </h3>
+     lien :  <a href="https://www.youtube.com/watch?v=pKdMhkciwrs&list=PL6urkeK7KgD41JDZRHaQzdKlHrJ5-fvdr&index=1">Cliquez ici pour accéder à la page d'exemple</a>  <!-- creation du lien -->
+      <br>
+
+      <form action="ajouter_au_panier.php" method="post">
+  <input type="hidden" name="product_id" value="piano">
+<button type="submit">Ajouter au panier</button> <!-- Creation d'un bouton ajouter au panier -->
+</form>  <!-- Creation d'un bouton ajouter au panier -->
+<hr>
   </body>
 </html>
