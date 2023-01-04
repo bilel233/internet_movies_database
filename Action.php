d694919c959929608c33d0ce61c38bc42c075f84
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-04 10:36:50 +0100

    Create Action.php

---
+++ Action.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Titre de la page</title>
+    <?php echo '<link rel="stylesheet" href="styleAction.css">'; ?>
+    <meta charset="UTF-8">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Titre de la page</h1>
+    <p>Contenu de la page</p>
+  </body>
+</html>
