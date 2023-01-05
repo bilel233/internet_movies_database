ba81c3aae72a3b3067e1bce4a617d180d21a5c87
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-05 19:30:34 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,13 +1,12 @@
 <?php
-$_POST["nom"];
-print_r($_POST);
+// Récupérer les données du formulaire sous forme ternaire
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
 
-
-//============================================
-
-  
-
-
-
+// Afficher les données du formulaire
+echo "Nom : $name<br>";
+echo "Email : $email<br>";
+echo "Message : $message<br>";
 ?>
 
