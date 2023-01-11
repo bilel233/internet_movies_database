de36ea2f97dbb484c708c428c0ba64b8389f2bb9
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-11 11:26:18 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,7 +1,25 @@
 
 <?php
-$search_query = $_GET['query'];
+
+$keyword = $_GET['keyword'];
+
+/* on se connecte a la bdd */
+$bdd = new PDO("mysql:host=localhost;dbname=videos;charset=utf8", "root");
+/* requete sql pour rechercher dans la base de donnees les enregistrements de video correspondants au mot cle recherché */
+$stmt = $bdd->prepare('SELECT * FROM videos WHERE title LIKE :keyword OR description LIKE :keyword');
+$stmt->bindValue(':keyword', "%$keyword%", PDO::PARAM_STR);
+$stmt->execute();
+$results = $stmt->fetchAll();
 
 
+foreach ($results as $row) {
+    echo '<p>'.$row['title'].'</p>';
+ }
+ 
+
+
+
+ 
+ 
 
 ?>
