3a8d1d7113fee95038a35515604332dfd3eb298e
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-13 09:51:54 +0100

    Update monCompte.php

--- monCompte.php
+++ monCompte.php
@@ -1,27 +1,25 @@
 
 
 
-<?php
-
-$username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
-$email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
-$password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
-?>
-<form method="post" action="monCompte.php">
-    <label for="username">Nom d'utilisateur :</label>
-    <input type="text" name="username" id="username" required>
-    <br>
-    <label for="email">Adresse e-mail :</label>
-    <input type="email" name="email" id="email" required>
-    <br>
-    <label for="password">Mot de passe :</label>
-    <input type="password" name="password" id="password" required>
-    <br>
-    <label for="password_confirm">Confirmez le mot de passe :</label>
-    <input type="password" name="password_confirm" id="password_confirm" required>
-    <br>
-    <input type="submit" value="S'inscrire">
-</form>
-
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Registration Form</title>
+    <link rel="stylesheet" type="text/css" href="monCompte.css">
+  </head>
+  <body>
+    <div class="container">
+      <form action="monCompte.php" method="post">
+        <h1>Registration Form</h1>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <input type="submit" value="Register">
+      </form>
+    </div>
+  </body>
+</html>
 
