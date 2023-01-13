cf33d712fead96c2750fb8303ff36bc3eeb5a59e
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-13 14:53:58 +0100

    Create Accueil.php

---
+++ Accueil.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page d'accueil</title>
+    <style>
+      /* CSS styles go here */
+      body {
+        background-color: #f0f0f0;
+        font-family: Arial, sans-serif;
+      }
+      header {
+        background-color: #333333;
+        color: #ffffff;
+        padding: 20px;
+        text-align: center;
+      }
+      header h1 {
+        margin: 0;
+      }
+      main {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+      }
+      .card {
+        background-color: #ffffff;
+        padding: 20px;
+        margin-bottom: 20px;
+      }
+      .card img {
+        max-width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1>Accueil</h1>
+    </header>
+    <main>
+      <div class="card">
+        <h2>Mon site de streaming</h2>
+        
+      </div>
+      <div class="card">
+        <h2>types de films</h2>
+        <ul>
+          <li>Action</li>
+          <li>Drama</li>
+          
+          
+        </ul>
+      </div>
+      
+        
+        
+      </div>
+    </main>
+  </body>
+</html>
+
+
+<?php
+  // Connection à la BDD
+  $host = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "videos";
+  $conn = mysqli_connect($host, $username, $password, $dbname);
+  // Vérification de la connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  // Récupération des images de la BDD
+  $query = "SELECT * FROM images";
+  $result = mysqli_query($conn, $query);
+?>
+<main>
+    <?php while($row = mysqli_fetch_assoc($result)) { ?>
+    <div class="card">
+      <h2><?php echo $row['title']; ?></h2>
+      <img src="Images/<?php echo $row['title']; ?>" alt="<?php echo $row['description']; ?>">
+      <p><?php echo $row['description']; ?></p>
+    </div>
+    <?php } ?>
+  </main>
